3485eb3aa5a34b0211787a087789e20a27df5436
amunyana <67626756+amunyana@users.noreply.github.com>
2021-06-12 23:11:50 -0700

    Update babblingscript.js

--- babblingscript.js
+++ babblingscript.js
@@ -103,7 +103,7 @@ play.addEventListener('click', playPauseMedia);
 disc.addEventListener('play', updatePlayPauseIcon);
 disc.addEventListener('pause', updatePlayPauseIcon);
 disc.addEventListener('timeupdate', updateProgress);
-disc.addEventListener(pause, 'ended');
+disc.addEventListener('ended');
 
 // Go to next song when next button clicked
 prev.addEventListener('click', gotoPreviousSong);
